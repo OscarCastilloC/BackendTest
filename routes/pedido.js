e17a0f30955a6f33c378803bfd32f280df3d6d7a
OscarCastilloC <113255829+OscarCastilloC@users.noreply.github.com>
2024-06-13 07:32:04 -0500

    Update pedido.js

--- routes/pedido.js
+++ routes/pedido.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const pedidoController = require('../controllers/pedidoController');
 
 router.post('/registrar_pedido',pedidoController.registrar_pedido);
-router.get('/obtener_pedido/:filtro ',pedidoController.obtener_pedido);
+router.get('/obtener_pedido/:filtro',pedidoController.obtener_pedido);
 
 module.exports = router;
