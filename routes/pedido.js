ae4346afef818167df92d58ed40e91b964168fd7
Oscar <oscarcastillocaja@gmail.com>
2024-06-13 03:27:22 -0500

    g

--- routes/pedido.js
+++ routes/pedido.js
@@ -4,3 +4,6 @@ const router = express.Router();
 const pedidoController = require('../controllers/pedidoController');
 
 router.post('/registrar_pedido',pedidoController.registrar_pedido);
+
+
+module.exports = router;
